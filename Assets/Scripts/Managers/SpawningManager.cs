aaaf7b15dc784a28b927716a4c9a72b2c5ead126
Mick M <mickwpmcdonnell@gmail.com>
2020-04-19 16:09:16 +1000

    Updated spawner to only spawn inside game area

--- Assets/Scripts/Managers/SpawningManager.cs
+++ Assets/Scripts/Managers/SpawningManager.cs
@@ -11,24 +11,33 @@ public class SpawningManager : MonoBehaviour
     public PlayNode playNodePrefab;
     public TrainNode trainNodePrefab;
 
+    public RectTransform gameArea;
+
+    Vector2 GetTopRightGameAreaWorldSpace(float xOffset = 0, float yOffset = 0)
+    {
+        Vector2 topLeft = new Vector2(-gameArea.sizeDelta.x + xOffset, Screen.height + gameArea.sizeDelta.y - yOffset);
+        Debug.Log($"Topleft = {topLeft}");
+        return Camera.main.ScreenToWorldPoint(topLeft);
+    }
+
 
 
     Vector3 GetNewSpawnPos()
     {
         //Dont use the FULL edge of the screen - leave room for sprite size
-        float xOffset = Screen.width * 0.1f;
-        float yOffset = Screen.height * 0.1f;
-        Vector2 originToWorld = Camera.main.ScreenToWorldPoint(new Vector2(xOffset, yOffset));
-        Vector2 screenEdgeToWorld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width - xOffset, Screen.height - yOffset));
+        float xOffset = Screen.width * 0.05f;
+        float yOffset = Screen.height * 0.05f;
+        Vector2 originToWorld = GetTopRightGameAreaWorldSpace(xOffset, yOffset);
+        Vector2 screenEdgeToWorld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width - xOffset, yOffset));
 
         //TODO: Physics2D.OverlapCollider for all nodes in the scene to make sure it doesnt double up?
 
         return new Vector3(
             Random.Range(originToWorld.x, screenEdgeToWorld.x),
-            Random.Range(originToWorld.y, screenEdgeToWorld.y), 
+            Random.Range(originToWorld.y, screenEdgeToWorld.y),
             0);
     }
-
+    
     bool CanSpawn(Nodes node)
     {
         return GameManager.instance.CanAfford(node);
---
+++ Assets/SpawnTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTest : MonoBehaviour
+{
+    public RectTransform gameArea;
+
+    Vector2 GetTopRightGameAreaWorldSpace(float xOffset = 0, float yOffset = 0)
+    {
+        Vector2 topLeft = new Vector2(-gameArea.sizeDelta.x + xOffset, Screen.height + gameArea.sizeDelta.y - yOffset);
+        Debug.Log($"Topleft = {topLeft}");
+        return Camera.main.ScreenToWorldPoint(topLeft);
+    }
+
+
+
+    Vector3 GetNewSpawnPos()
+    {
+        //Dont use the FULL edge of the screen - leave room for sprite size
+        float xOffset = Screen.width * 0.05f;
+        float yOffset = Screen.height * 0.05f;
+        Vector2 originToWorld = GetTopRightGameAreaWorldSpace(xOffset, yOffset);
+        Vector2 screenEdgeToWorld = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width - xOffset, yOffset));
+
+        //TODO: Physics2D.OverlapCollider for all nodes in the scene to make sure it doesnt double up?
+
+        return new Vector3(
+            Random.Range(originToWorld.x, screenEdgeToWorld.x),
+            Random.Range(originToWorld.y, screenEdgeToWorld.y),
+            0);
+    }
+
+    public GameObject prefabToSpawn;
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Input.mousePosition);
+            Instantiate(prefabToSpawn, GetNewSpawnPos(), Quaternion.identity);
+        }
+    }
+}
